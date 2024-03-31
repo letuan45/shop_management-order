9b32b3158442d33426b6e683c0c74af2468c49b3
Tuấn Lê <n19dccn177@student.ptithcm.edu.vn>
2024-03-31 22:28:03 +0700

    feat: supplier management

--- src/supplier/supplier.controller.ts
+++ src/supplier/supplier.controller.ts
@@ -8,6 +8,16 @@ import { UpdateSupplierDto } from './dtos/updateSupplier.dto';
 export class SupplierController {
   constructor(private supplierService: SupplierService) {}
 
+  @MessagePattern({ cmd: 'get_supplier' })
+  async get(data: { page: number; search?: string }) {
+    return await this.supplierService.get(data.page, data.search);
+  }
+
+  @MessagePattern({ cmd: 'get_supplier_by_id' })
+  async getById(data: { id: number }) {
+    return await this.supplierService.getById(data.id);
+  }
+
   @MessagePattern({ cmd: 'create_supplier' })
   async create(createSupplierDto: CreateSupplierDto) {
     return await this.supplierService.create(createSupplierDto);
--- src/supplier/supplier.service.ts
+++ src/supplier/supplier.service.ts
@@ -7,10 +7,50 @@ import { SupplierRepository } from './supplier.repository';
 import { CreateSupplierDto } from './dtos/createSupplier.dto';
 import { RpcException } from '@nestjs/microservices';
 import { UpdateSupplierDto } from './dtos/updateSupplier.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SupplierService {
-  constructor(private supplierRepository: SupplierRepository) {}
+  constructor(
+    private supplierRepository: SupplierRepository,
+    private prisma: PrismaService,
+  ) {}
+
+  pageLimit = 10;
+
+  async get(page: number, search?: string) {
+    const skip = this.pageLimit * (page - 1);
+    let where = {};
+    if (search) {
+      where = {
+        OR: [{ name: { contains: search } }, { phone: { contains: search } }],
+      };
+    }
+
+    const data = await this.prisma.supplier.findMany({
+      take: this.pageLimit,
+      skip: skip,
+      where: where,
+      orderBy: { id: 'desc' },
+    });
+
+    const count = await this.prisma.supplier.count({ where: where });
+
+    return {
+      data,
+      total: count,
+    };
+  }
+
+  async getById(id: number) {
+    const supplier = await this.supplierRepository.getById(id);
+    if (!supplier) {
+      throw new RpcException(
+        new NotFoundException('không tìm thấy nhà cung cấp!'),
+      );
+    }
+    return supplier;
+  }
 
   async create(createSupplierDto: CreateSupplierDto) {
     if (await this.supplierRepository.getByEmail(createSupplierDto.email)) {
